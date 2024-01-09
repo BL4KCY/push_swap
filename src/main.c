406009720212667116e64c09faaa1dcb3450956f
BL4KCY <mostafarssiui@gmail.com>
2024-01-09 22:00:36 +0100

    update code

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 15:38:58 by melfersi          #+#    #+#             */
-/*   Updated: 2024/01/09 18:02:27 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/01/09 21:57:47 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,22 @@
 int	main(void)
 {
 	int stack_a[6] = {1,2,3,4,5};
-	int stack_b[6];// = {6,7,8,9,10};
+	//int stack_b[6];// = {6,7,8,9,10};
 
-	int size_a = 6;//sizeof(stack_a) / sizeof(stack_a[0]);
-	int size_b = 1;//sizeof(stack_b) / sizeof(stack_b[0]);
+	int size_a = 5;//sizeof(stack_a) / sizeof(stack_a[0]);
+	//int size_b = 0;//sizeof(stack_b) / sizeof(stack_b[0]);
 
-	SA(stack_a, size_a);
-	for (int i = 0; i < size_a ;i++)
-		ft_putnbr_fd(stack_a[i], 1);
-	ft_putendl_fd("",1);
-	SA(stack_a, size_a);
-	for (int i = 0; i < size_a ;i++)
-		ft_putnbr_fd(stack_a[i], 1);
+	for (int i = 0; i < size_a;i++)
+		ft_printf("%d",stack_a[i]);
 
-	ft_putendl_fd("",1);
+	rotate(stack_a, size_a);
+	rrotate(stack_a, size_a);
 
-	SB(stack_b, size_b);
-	for (int i = 0; i < size_b ;i++)
-		ft_putnbr_fd(stack_b[i], 1);
-	ft_putendl_fd("",1);
-	SB(stack_b, size_b);
-	for (int i = 0; i < size_b ;i++)
-		ft_putnbr_fd(stack_b[i], 1);
+	ft_puts("");
 
-	ft_putendl_fd("",1);
-	SS(stack_a, stack_b, size_a, size_b);
-	for (int i = 0; i < size_a ;i++)
-		ft_putnbr_fd(stack_a[i], 1);
-	ft_putendl_fd("",1);
-	for (int i = 0; i < size_b ;i++)
-		ft_putnbr_fd(stack_b[i], 1);
+	for (int i = 0; i < size_a;i++)
+		ft_printf("%d",stack_a[i]);
 
-	ft_putendl_fd("",1);
-	PA(stack_a, stack_b, &size_a, &size_b);
-	for (int i = 0; i < size_a ;i++)
-		ft_putnbr_fd(stack_a[i], 1);
-	ft_putendl_fd("",1);
-	for (int i = 0; i < size_b ;i++)
-		ft_putnbr_fd(stack_b[i], 1);
 
 
 
