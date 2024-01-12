b16f61593778696a74938f6116e4fc5726da53fb
BL4KCY <mostafarssiui@gmail.com>
2024-01-12 11:34:47 +0100

    Update swap, push, rotate, and reverse rotate functions

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 09:57:55 by melfersi          #+#    #+#             */
-/*   Updated: 2024/01/10 10:43:46 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/01/12 10:15:33 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,46 +19,49 @@
 /*---------------------- Macro Functions ----------------------*/
 
 // Swap the top elements of stack A and print "sa"
-# define SA(A, B)(swap(A, B), ft_puts("sa"))
+# define SA(A)(swap(A), ft_puts("sa"))
 
 // Swap the top elements of stack B and print "sb"
-# define SB(B, A)(swap(B, A), ft_puts("sb"))
+# define SB(B)(swap(B), ft_puts("sb"))
 
 // Swap the top elements of both stacks A and B, and print "ss"
-# define SS(A, B, SZA, SZB)(swap(A, SZA),swap(B, SZB),ft_puts("ss"))
+# define SS(A, B)(swap(A),swap(B),ft_puts("ss"))
 
 // Push the top element of stack A to stack B, and print "pa"
-# define PA(A, B, size_A, size_B)(push(A, B, size_A, size_B),ft_puts("pa"))
+# define PA(A, B)(push(A, B),ft_puts("pa"))
 
 // Push the top element of stack B to stack A, and print "pb"
-# define PB(B, A, size_B, size_A)(push(B, A, size_B, size_A),ft_puts("pb"))
+# define PB(B, A)(push(B, A),ft_puts("pb"))
 
 // Rotate stack A and print "ra"
-# define RA(stack, size)(rotate(stack, size),ft_puts("ra"))
+# define RA(A)(rotate(A),ft_puts("ra"))
 
 // Rotate stack B and print "rb"
-# define RB(stack, size)(rotate(stack, size),ft_puts("rb"))
+# define RB(B)(rotate(B),ft_puts("rb"))
 
 // Rotate both stacks A and B, and print "rr"
-# define RR(A, B, SZA, SZB)(rotate(A,SZA),rotate(B,SZB),ft_puts("rr"))
+# define RR(A, B)(rotate(A),rotate(B),ft_puts("rr"))
 
 // Reverse rotate stack A and print "rra"
-# define RRA(stack, size)(rrotate(stack, size),ft_puts("rra"));
+# define RRA(A)(reverse_rotate(A),ft_puts("rra"));
 
 // Reverse rotate stack B and print "rrb"
-# define RRB(stack, size)(rrotate(stack, size),ft_puts("rrb"));
+# define RRB(B)(reverse_rotate(B),ft_puts("rrb"));
 
 // Reverse rotate both stacks A and B, and print "rrr"
-# define RRR(A, B, SZA, SZB)(rrotate(A,SZA),rrotate(B,SZB),ft_puts("rrr"))
+# define RRR(A, B)(reverse_rotate(A),reverse_rotate(B),ft_puts("rrr"))
+
+// Print an error message to standard error output
+# define ERR()(ft_putendl_fd("Error", STDERR_FILENO),exit(EXIT_FAILURE))
 
 /*---------------------- Prototypes ----------------------*/
 
-void	swap(int *stack, int size);
+void	push(t_list **src, t_list **dst);
 
-void	push(int *dst, int *src, int *sz_dst, int *sz_src);
+void	swap(t_list **lst);
 
-void	rotate(int *stack, int size);
+void	rotate(t_list **lst);
 
-void	rrotate(int *stack, int size);
+void	reverse_rotate(t_list **lst);
 
 #endif
