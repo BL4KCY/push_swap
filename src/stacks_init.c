0250809fcecf9e5d6608483496f6d386e5c08b32
BL4KCY <mostafarssiui@gmail.com>
2024-01-13 10:25:04 +0100

    Refactor argument parsing and stack initialization

--- src/stacks_init.c
+++ src/stacks_init.c
@@ -6,22 +6,83 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 11:47:56 by melfersi          #+#    #+#             */
-/*   Updated: 2024/01/10 18:16:26 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/01/13 10:21:09 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
-int	is_args_valid(char **args)
+void	parse_args(int ac, char **av, t_list **stack_a)
 {
-	
+	int		i;
+	int		j;
+	char	**split;
+	int		*value;
+	i = 0;
+	while (++i < ac)
+	{
+		split = ft_split(av[i], ' ');
+		j = 0;
+		while (split[j] || (!j && !split[j]))
+		{
+			value = malloc(sizeof(int));
+			if (!is_valid_arg(split[j], *stack_a) || !value || (!j && !split[j]))
+			{
+				if (value)
+					free(value);
+				ft_free_split(split);
+				ft_lstclear(stack_a, free);
+				ERR();
+			}
+			*value = ft_atoi(split[j++]);
+			ft_lstadd_back(stack_a, ft_lstnew(value));
+		}
+		ft_free_split(split);
+	}
 }
 
-int	*init_stacks(char **args, int **stack_a, int **size)
+bool	is_valid_arg(char *arg, t_list *stack_a)
 {
 	int	i;
-	int *stack_b;
+	int	sign;
 
+	i = -1;
+	sign = 0;
+	if (!arg)
+		return (false);
+	if (exist(stack_a, ft_atoi(arg)))
+		return (false);
+	while (arg[++i])
+	{
+		if (!ft_isdigit(arg[i]) && arg[i] != '-' && arg[i] != '+' && arg[i] != ' ')
+			return (false);
+		if(arg[i] == '-' || arg[i] == '+')
+			sign++;
+		if (sign > 1)
+			return (false);
+	}
+	if ((ft_atoi(arg) > INT_MAX || ft_atoi(arg) < INT_MIN) || !i)
+		return (false);
+	return (true);
+}
 
+bool	exist(t_list *stack, int value)
+{
+	while (stack)
+	{
+		if (*(int *)stack->content == value)
+			return (true);
+		stack = stack->next;
+	}
+	return (false);
+}
+
+void	ft_free_split(char **split)
+{
+	int	i;
+
+	i = -1;
+	while (split[++i])
+		free(split[i]);
+	free(split);
 }
