ef5d59e44c5dc2d507e8051da36565ac14722161
BL4KCY <mostafarssiui@gmail.com>
2024-01-15 21:36:17 +0100

    Refactor stack initialization and argument parsing
    The commit message suggests that the changes made in the code involve refactoring the stack initialization and argument parsing functions.

--- src/stacks_init.c
+++ src/stacks_init.c
@@ -6,18 +6,18 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 11:47:56 by melfersi          #+#    #+#             */
-/*   Updated: 2024/01/13 17:19:42 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/01/15 16:07:12 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	parse_args(int ac, char **av, t_list **stack_a)
+void	parse_args(int ac, char **av, t_stack **stack_a)
 {
 	int		i;
 	int		j;
 	char	**split;
-	int		*value;
+
 	i = 0;
 	while (++i < ac)
 	{
@@ -25,23 +25,19 @@ void	parse_args(int ac, char **av, t_list **stack_a)
 		j = 0;
 		while (split[j] || (!j && !split[j]))
 		{
-			value = malloc(sizeof(int));
-			if (!is_valid_arg(split[j], *stack_a) || !value || (!j && !split[j]))
+			if (!is_valid_arg(split[j], *stack_a) || (!j && !split[j]))
 			{
-				if (value)
-					free(value);
 				ft_free_split(split);
-				ft_lstclear(stack_a, free);
+				ft_lstclear(stack_a);
 				ERR();
 			}
-			*value = ft_atoi(split[j++]);
-			ft_lstadd_back(stack_a, ft_lstnew(value));
+			ft_lstadd_back(stack_a, ft_lstnew(ft_atoi(split[j++])));
 		}
 		ft_free_split(split);
 	}
 }
 
-bool	is_valid_arg(char *arg, t_list *stack_a)
+bool	is_valid_arg(char *arg, t_stack *stack_a)
 {
 	int	i;
 	int	sign;
@@ -84,4 +80,20 @@ bool	is_valid_num(char *arg)
 	}
 	free(tmp);
 	return (true);
+}
+
+void	init_a_nodes(t_stack *a, t_stack *b)
+{
+	current_index(a);
+	current_index(b);
+	set_target_a(a, b);
+	cost_to_a(a, b);
+	set_cheapest(a);
+}
+
+void	init_b_nodes(t_stack *a, t_stack *b)
+{
+	current_index(a);
+	current_index(b);
+	set_target_b(a, b);
 }
