6a0b47ae57a1117ddefae9a0b00dc9539649b4ce
BL4KCY <mostafarssiui@gmail.com>
2024-01-15 21:36:00 +0100

    Refactor push_swap function to improve code readability and maintainability

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 15:38:58 by melfersi          #+#    #+#             */
-/*   Updated: 2024/01/13 22:15:17 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/01/15 12:03:00 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,34 +27,22 @@ int main(int ac, char **av)
 
 void	push_swap(int ac, char **av)
 {
-	t_list	*stack_a;
-	t_list	*stack_b;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 
 	stack_a = NULL;
 	stack_b = NULL;
 	if (ac == 1)
 		return ;
 	parse_args(ac, av, &stack_a);
-	if (is_sorted(stack_a))
+	if (!is_sorted(stack_a))
 	{
-		ft_lstclear(&stack_a, free);
-		return ;
+		if (ft_lstsize(stack_a) == 2)
+			SA(&stack_a);
+		else if (ft_lstsize(stack_a) == 3)
+			sort_three(&stack_a);
+		else
+			sort_stacks(&stack_a, &stack_b);
 	}
-	if (ft_lstsize(stack_a) == 2)
-		SA(&stack_a);
-	else if (ft_lstsize(stack_a) == 3)
-		sort_three(&stack_a);
-	else if (ft_lstsize(stack_a) == 4)
-		sort_four(&stack_a, &stack_b);
-	else if (ft_lstsize(stack_a) == 5)
-		sort_five(&stack_a, &stack_b);
-	// else
-	// 	sort(&stack_a, &stack_b);
-	// ft_lstclear(&stack_a, free);
-	// ft_lstclear(&instructions, free);
-
-	// ft_printf("stack a:\n");
-	// ft_lstiter(stack_a, print);
-	ft_lstclear(&stack_a, free);
+	ft_lstclear(&stack_a);
 }
-
