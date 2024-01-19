af01de0df78d04364b8314e48aaeaee8914d0603
BL4KCY <mostafarssiui@gmail.com>
2024-01-19 11:28:06 +0100

    Update function prototypes in push_swap.h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 09:57:55 by melfersi          #+#    #+#             */
-/*   Updated: 2024/01/19 10:39:27 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/01/19 11:27:20 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,60 +57,88 @@
 
 /*---------------------- Prototypes ----------------------*/
 
+// Function to push the top element from src stack to dst stack
 void	push(t_stack **src, t_stack **dst);
 
+// Function to swap the top two elements of a stack
 void	swap(t_stack **lst);
 
+// Function to rotate the stack by moving the top element to the bottom
 void	rotate(t_stack **lst);
 
+// Function to reverse rotate the stack by moving the bottom element to the top
 void	reverse_rotate(t_stack **lst);
 
+// Function to parse command line arguments and create stack_a
 void	parse_args(int ac, char **av, t_stack **stack_a);
 
+// Function to free the memory allocated for a string array
 void	ft_free_split(char **split);
 
+// check if an argument is a valid integer and not already present in stack_a
 bool	is_valid_arg(char *arg, t_stack *stack_a);
 
+// Function to check if a value exists in the stack
 bool	exist(t_stack *stack, int value);
 
+// Function to check if the stack is sorted in ascending order
 bool	is_sorted(t_stack *stack);
 
+// Function to sort a stack of three elements
 void	sort_three(t_stack **stack_a);
 
+// Function to sort the stacks using the push_swap algorithm
 void	sort_stacks(t_stack **a, t_stack **b);
 
+// Function to assign current index to each node in the stack
 void	current_index(t_stack *stack);
 
+// Function to set the target index for each node in stack_a
 void	set_target_a(t_stack *a, t_stack *b);
 
+// Function to set the target index for each node in stack_b
 void	set_target_b(t_stack *a, t_stack *b);
 
+// Function to calculate the cost of moving each node from stack_b to stack_a
 void	cost_to_a(t_stack *a, t_stack *b);
 
+// Function to set the cheapest node to move from stack_b to stack_a
 void	set_cheapest(t_stack *stack);
 
+// Function to push the top element from stack_b to stack_a
 void	push_a(t_stack **a, t_stack **b);
 
+// Function to push the top element from stack_a to stack_b
 void	push_b(t_stack **a, t_stack **b);
 
+// Function to initialize the nodes in stack_a with default values
 void	init_a_nodes(t_stack *a, t_stack *b);
 
+// Function to initialize the nodes in stack_b with default values
 void	init_b_nodes(t_stack *a, t_stack *b);
 
+// Function to move the minimum value to the top of the stack
 void	min_to_top(t_stack **stack);
 
+// arrange the stack by moving the top element to the specified position
 void	arrange(t_stack **stack, t_stack *top, char c);
 
+// Function to get the node with the cheapest value in the stack
 t_stack	*get_cheapest(t_stack *stack);
 
+// Function to get the node with the minimum value in the stack
 t_stack	*get_min(t_stack *stack);
 
+// Function to get the node with the maximum value in the stack
 t_stack	*get_max(t_stack *stack);
 
+// Function to check if a string argument is a valid number
 bool	is_valid_num(char *arg);
 
+// rotate both stacks A and B by moving the top elements to the bottom
 void	both_rrotate(t_stack **a, t_stack **b, t_stack *top);
 
+// rotate both stacks A and B by moving the bottom elements to the top
 void	both_rotate(t_stack **a, t_stack **b, t_stack *top);
 
 #endif
